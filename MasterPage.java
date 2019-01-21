f51129319d26d7f71d9af6ba126a404de61e04b2
Vipul195 <46863297+Vipul195@users.noreply.github.com>
2019-01-21 10:45:51 +0530

    Add files via upload

---
+++ MasterPage.java
@@ -0,0 +1,49 @@
+package org.openweathermap.pages.master;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class MasterPage {
+
+	public WebDriver driver = null;
+	public Properties prop = null;
+	
+	public MasterPage(WebDriver driver,  String propFilePath ) throws IOException {
+		this.driver = driver;
+		
+
+		FileInputStream ip = new FileInputStream("F:\\Java Class\\workspace\\ErosScreening-master\\src\\test\\resources\\home.locators.properties");
+		this.prop = new Properties();
+		this.prop.load(ip);
+		
+	}
+	
+	/**
+	 * 
+	 * @param xpathKeys
+	 * @param userData
+	 * Place to write common methods in order to interact with web elements.
+	 */
+	
+	public void sendKeys(String xpathKeys, String userData)
+	{
+		this.driver.findElement(By.xpath(this.prop.getProperty(xpathKeys))).sendKeys(userData);
+	}
+	public boolean elementDisplayed(String xpathkeys)
+	{
+		return this.driver.findElement(By.xpath(this.prop.getProperty(xpathkeys))).isDisplayed();
+	}
+	public void click(String xpathkeys)
+	{
+		this.driver.findElement(By.xpath(this.prop.getProperty(xpathkeys))).click();
+	}
+	public void getText(String xpathkeys)
+	{
+		this.driver.findElement(By.xpath(this.prop.getProperty(xpathkeys))).getText();
+	}
+	
+}
---
+++ ValidCityTest.java
@@ -0,0 +1,35 @@
+package org.openweathermap;
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import java.io.IOException;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openweathermap.pages.HomePage;
+import org.openweathermap.test.base.BaseTestClass;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ValidCityTest extends BaseTestClass{
+
+private HomePage homePage = null;
+	
+	
+	@Test
+	public void validCityName() throws IOException {
+		this.homePage = new HomePage(this.driver);
+		WebDriverWait wait1 = new WebDriverWait(driver, 30);
+		WebElement search = wait1.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@placeholder='Your city name']")));
+		this.homePage.enterCity("Agra");
+		List<WebElement> company = driver.findElements(By.xpath("//*[@id='forecast_list_ul']/table/tbody/tr/td[2]"));
+		if(company.size()>0)
+		{
+			driver.findElement(By.xpath("//*[@id='forecast_list_ul']/table/tbody/tr[1]/td[2]/b[1]/a")).click();
+		}
+		Assert.assertTrue(this.homePage.isWeatherDisplayed());
+	}
+}
